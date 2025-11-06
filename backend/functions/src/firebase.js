d6b101d6f1375e375ff70f0bc8a7fa4e7ee25f0f
Hasee28 <tohazsha@gmail.com>
2025-11-06 12:18:08 +0530

    Connected database successfully

---
+++ backend/functions/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC53sa8nPeQA68X5FmgSTvLJmrc_AI_LSo",
+  authDomain: "hadith-master-40045.firebaseapp.com",
+  projectId: "hadith-master-40045",
+  storageBucket: "hadith-master-40045.appspot.com",
+  messagingSenderId: "956247344001",
+  appId: "1:956247344001:web:3f877101e43196426e5e80",
+  measurementId: "G-N10ZYSJ709"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore (for storing Hadith data)
+export const db = getFirestore(app);
